5e9cadfdbc8708734f266e455452d5a70615ec1d
francescocastelli <francesco8.castelli@mail.polimi.it>
2022-03-10 23:01:01 +0100

    fix merge sort

--- benchmarks/algorithms/mergeSortBM.cpp
+++ benchmarks/algorithms/mergeSortBM.cpp
@@ -4,24 +4,20 @@
 #include <benchmark/benchmark.h>
 #include "sorting.h"
 
-static void BM_mergeSort(benchmark::State& state) 
+static void BM_mergeSort_avg_case(benchmark::State& state) 
 {
-    std::vector<int> v; 
-
-    for (int i=0; i<state.range(0); i++)
-    {
-        v.push_back(rand() % 100);
-    }
+    std::vector<int> v (state.range(0));
+    std::iota(v.begin(), v.end(), 2);
 
     for (auto _ : state)
     {
-        //current function under benchmarking
         benchmark::DoNotOptimize(sorting::mergeSort(v));
     }
     state.SetComplexityN(state.range(0));
 
 }
 
-BENCHMARK(BM_mergeSort)->RangeMultiplier(2)->Range(1<<2, 1<<20)->Complexity();
+BENCHMARK(BM_mergeSort_avg_case)->RangeMultiplier(2)->Range(1<<2, 1<<20)
+                                ->Complexity(benchmark::oNLogN);
 
 BENCHMARK_MAIN();
--- src/algorithms/sorting/mergeSort.cpp
+++ src/algorithms/sorting/mergeSort.cpp
@@ -4,29 +4,31 @@ namespace sorting
 {
 std::vector<int> _merge(std::vector<int> &v1, std::vector<int> &v2)
 {
-    int i = 0, j = 0, k = 0;
+    int i = 0, j = 0;
     std::vector<int> v;
+    v.reserve(v1.size() + v2.size());
 
-    while ( i < v1.size() && j < v2.size() )
+    while (i < v1.size() && j < v2.size())
     {
         if ( v1[i] <= v2[j] )
         {
             v.push_back(v1[i]);
             i++;
-        }else 
+        }
+        else 
         {
             v.push_back(v2[j]); 
             j++;
         }
     }
 
-    while ( i < v1.size() )
+    while (i < v1.size())
     {
         v.push_back(v1[i]);
         i++;
     }
 
-    while( j < v2.size() )
+    while(j < v2.size())
     {
         v.push_back(v2[j]); 
         j++;
@@ -37,16 +39,21 @@ std::vector<int> _merge(std::vector<int> &v1, std::vector<int> &v2)
 
 int mergeSort(std::vector<int> &v)
 {
-    std::vector<int> v1, v2;
-    int n = v.size();
-
-    if ( n == 1 ) return 0;
+    if (v.size() == 1) return 0;
     
-    for ( int i=0; i<v.size()/2; i++ )
-        v1.push_back(v[i]);
+    std::vector<int> v1, v2;
+    v1.reserve(v.size()/2);
+    v2.reserve(v.size()/2);
 
-    for ( int i=v.size()/2; i<v.size(); i++ )
+    for (int i=0; i<v.size()/2; i++)
+    {
+        v1.push_back(v[i]);
+    }
+
+    for (int i=v.size()/2; i<v.size(); i++)
+    {
         v2.push_back(v[i]);
+    }
 
     mergeSort(v1);
     mergeSort(v2);
